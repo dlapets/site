030351c1d361e63f615a529bb5dbcfcb47536ef7
dlapets <lapets@adjust.com>
2026-03-28 18:00:25 +0100

    Insert some junk into db

---
+++ testdata/test_insert.sql
@@ -0,0 +1,8 @@
+insert into photo (name, description, taken_at, created_at) values 
+
+('DSC01852.jpg', '', current_timestamp, current_timestamp),
+('DSC02056.jpg', 'sensor was dirty', current_timestamp, current_timestamp),
+('img394.jpg', 'something on film', current_timestamp, current_timestamp),
+('img558.jpg', '', current_timestamp, current_timestamp),
+('R0000503.jpg', 'I broke this camera later!', current_timestamp, current_timestamp)
+;
