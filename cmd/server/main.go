76da8f58491402e2ca90bc3ae68a97cbad0164fc
dlapets <lapets@adjust.com>
2026-03-28 19:16:35 +0100

    Add env reader and simple file server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -10,10 +13,41 @@ const (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicf("failed to load .env file: %s", err)
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		log.Panicf("missing SERVER_PORT")
+	}
+	log.Printf("server port: %s", port)
+
+	staticDir := os.Getenv("SERVER_STATIC_CONTENT_DIR")
+	if staticDir == "" {
+		log.Panicf("missing SERVER_STATIC_CONTENT_DIR")
+	}
+	log.Printf("static content dir: %s", staticDir)
+
+	// Set up handlers
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(Port, nil))
+
+	// Use file server for image files/etc. for now.
+	http.Handle(
+		"/images/{name}",
+		http.StripPrefix("/images/", http.FileServer(http.Dir(staticDir))),
+	)
+
+	// Serve
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("GOT A REQUEST FOR 1", r)
 	w.Write([]byte("fdsafdas"))
+}
+func handler2(w http.ResponseWriter, r *http.Request) {
+	log.Println("GOT A REQUEST FOR 2", r)
+	w.Write([]byte("RUIEOWRUIEOW"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dlapets/site
 
 go 1.23
+
+require github.com/joho/godotenv v1.5.1
